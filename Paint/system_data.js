74cdf812f214326b06dcb973e17c2cd5cef8d6ed
67070018Chanyaphat <67070018@kmitl.ac.th>
2025-11-20 12:52:47 +0000

    Auto-update stats: 12:52:46

---
+++ Paint/system_data.js
@@ -0,0 +1,11 @@
+window.updateDashboard({
+  "time": "12:52:46",
+  "cpu": 9,
+  "memPercent": 12,
+  "memUsed": "0.5",
+  "memTotal": 4,
+  "disk": 48,
+  "netDown": "0.00",
+  "netUp": "0.00",
+  "temp": 0
+});
