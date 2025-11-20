d7af5737fc5ecc8bd7772bfc56a95d3e818bfdde
Yossaphol <yossaphol3502@gmail.com>
2025-11-20 12:18:39 +0000

    add index file and edit 67070145.html

--- 67070145/67070145_script.sh
+++ 67070145/67070145_script.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-HTML_FILE="/home/kenkungkab/Documents/miniproject/67070145/67070145.html"
+TEMPLATE_FILE="/home/kenkungkab/miniproject/67070145/67070145.html"
+OUTPUT_FILE="/home/kenkungkab/miniproject/67070145/yossaphol.html"
 
-# เก็บข้อมูล
+# เก็บข้อมูลระบบ
 CPU=$(top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8}')
-MEM=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
+MEM=$(free | grep Mem | awk '{printf "%.0f", $3/$2*100}')
 STORAGE=$(df -h / | awk 'NR==2 {print $5}')
 UPDATED=$(date "+%Y-%m-%d %H:%M:%S")
+
+# top 5 process
 TOP_PROC=$(ps -eo pid,user,%cpu,%mem,comm --sort=-%cpu | head -n 6 | tail -n 5)
 TOP_TABLE=""
-while read -r PID USER CPU MEM COMM; do
-    TOP_TABLE+="<tr><td>$PID</td><td>$USER</td><td>$CPU</td><td>$MEM</td><td>$COMM</td></tr>"
+while read -r PID USER CPU_P MEM_P COMM; do
+    TOP_TABLE+="<tr><td>$PID</td><td>$USER</td><td>$CPU_P%</td><td>$MEM_P%</td><td>$COMM</td></tr>"
 done <<< "$TOP_PROC"
 
-# เขียนข้อมูลลง HTML โดยใช้ sed แทนค่าภายใน span
-sed -i "s|<span id=\"cpu\">.*</span>|<span id=\"cpu\">$CPU%</span>|" "$HTML_FILE"
-sed -i "s|<span id=\"mem\">.*</span>|<span id=\"mem\">$MEM%</span>|" "$HTML_FILE"
-sed -i "s|<span id=\"storage\">.*</span>|<span id=\"storage\">$STORAGE</span>|" "$HTML_FILE"
-sed -i "s|<span id=\"lastupdate\">.*</span>|<span id=\"lastupdate\">$UPDATED</span>|" "$HTML_FILE"
-sed -i "s|<tbody id=\"top_process\">.*</tbody>|<tbody id=\"top_process\">$TOP_TABLE</tbody>|" "$HTML_FILE"
+# สร้าง HTML จาก template
+sed -e "s|{{CPU_USAGE}}|$CPU%|g" \
+    -e "s|{{MEM_USAGE}}|$MEM%|g" \
+    -e "s|{{STORAGE_USAGE}}|$STORAGE|g" \
+    -e "s|{{LAST_UPDATED}}|$UPDATED|g" \
+    -e "s|{{TOP_PROCESS_LIST}}|$TOP_TABLE|g" \
+    "$TEMPLATE_FILE" > "$OUTPUT_FILE"
+
